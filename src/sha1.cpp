d95bea1ea7ae80bf755d5c938947ee5891b9abdb
Mark Harmstone <mark@harmstone.com>
2022-06-01 17:51:53 +0100

    SHA1Final: make less insane

--- src/sha1.cpp
+++ src/sha1.cpp
@@ -164,32 +164,15 @@ static void SHA1Final(array<uint8_t, 20> digest, SHA1_CTX* context)
 	unsigned char finalcount[8];
 	unsigned char c;
 
-#if 0   /* untested "improvement" by DHR */
-	/* Convert context->count to a sequence of bytes
-	* in finalcount.  Second element first, but
-	* big-endian order within element.
-	* But we do it all backwards.
-	*/
-	unsigned char *fcp = &finalcount[8];
-
-	for (i = 0; i < 2; i++)
-	{
-		uint32_t t = context->count[i];
-		int j;
-
-		for (j = 0; j < 4; t >>= 8, j++)
-			*--fcp = (unsigned char) t;
-	}
-#else
 	for (i = 0; i < 8; i++) {
 		finalcount[i] = (unsigned char)((context->count[(i >= 4 ? 0 : 1)]
 			>> ((3-(i & 3)) * 8) ) & 255);  /* Endian independent */
 	}
-#endif
-	c = 0200;
+
+	c = 0x80;
 	SHA1Update(context, &c, 1);
-	while ((context->count[0] & 504) != 448) {
-		c = 0000;
+	while ((context->count[0] & 0x1f8) != 0x1c0) {
+		c = 0;
 		SHA1Update(context, &c, 1);
 	}
 	SHA1Update(context, finalcount, 8);  /* Should cause a SHA1Transform() */
