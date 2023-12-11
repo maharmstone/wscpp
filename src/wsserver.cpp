53ea20503283d9b33003f7903259db064e348d9c
Mark Harmstone <mark@harmstone.com>
2023-12-11 16:14:14 +0000

    server_client_pimpl::inflate_payload: fix warning

--- src/wsserver.cpp
+++ src/wsserver.cpp
@@ -890,15 +890,11 @@ namespace ws {
 
 			do {
 				strm.avail_in = (unsigned int)comp.size();
-
-				if (strm.avail_in == 0)
-					break;
-
 				strm.next_in = (uint8_t*)comp.data();
 
 				do {
 					if (strm.avail_in == 0)
-						break;
+						return;
 
 					strm.avail_out = sizeof(buf);
 					strm.next_out = buf;
