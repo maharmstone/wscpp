76263b1c85d6065c452ef5b79be8ac89a2d9666e
Mark Harmstone <mark@harmstone.com>
2023-12-11 14:54:15 +0000

    b64decode: make constexpr

--- src/b64.h
+++ src/b64.h
@@ -27,7 +27,7 @@ static constexpr char base64_table[65] =
 * Returns: Allocated buffer of out_len bytes of encoded data,
 * or empty string on failure
 */
-constexpr std::string b64encode(std::span<const uint8_t> sv) {
+static constexpr std::string b64encode(std::span<const uint8_t> sv) {
 	const unsigned char* src = (const unsigned char*)sv.data();
 	size_t len = sv.size();
 	char *out, *pos;
@@ -80,4 +80,49 @@ static_assert(b64encode(std::vector<uint8_t>{'f','o','o','b'}) == "Zm9vYg==");
 static_assert(b64encode(std::vector<uint8_t>{'f','o','o','b','a',}) == "Zm9vYmE=");
 static_assert(b64encode(std::vector<uint8_t>{'f','o','o','b','a','r'}) == "Zm9vYmFy");
 
-std::vector<uint8_t> b64decode(std::string_view sv);
+static constexpr int B64index[256] = {
+	0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 62, 63, 62, 62, 63,
+	52, 53, 54, 55, 56, 57, 58, 59, 60, 61,  0,  0,  0,  0,  0,  0,
+	0,   0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  0,  0,  0,  0, 63,
+	0,  26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+};
+
+static constexpr std::vector<uint8_t> b64decode(std::string_view sv) {
+	auto p = (char*)sv.data();
+	int pad = sv.length() > 0 && (sv.length() % 4 || p[sv.length() - 1] == '=');
+	const size_t L = ((sv.length() + 3) / 4 - pad) * 4;
+	std::vector<uint8_t> str;
+
+	str.resize(L / 4 * 3 + pad);
+
+	for (size_t i = 0, j = 0; i < L; i += 4) {
+		int n = B64index[(uint8_t)p[i]] << 18 | B64index[(uint8_t)p[i + 1]] << 12 | B64index[(uint8_t)p[i + 2]] << 6 | B64index[(uint8_t)p[i + 3]];
+		str[j++] = n >> 16;
+		str[j++] = n >> 8 & 0xFF;
+		str[j++] = n & 0xFF;
+	}
+
+	if (pad) {
+		int n = B64index[(uint8_t)p[L]] << 18 | B64index[(uint8_t)p[L + 1]] << 12;
+		str[str.size() - 1] = n >> 16;
+
+		if (sv.length() > L + 2 && p[L + 2] != '=') {
+			n |= B64index[(uint8_t)p[L + 2]] << 6;
+			str.push_back(n >> 8 & 0xFF);
+		}
+	}
+
+	return str;
+}
+
+static_assert(b64decode("") == std::vector<uint8_t>{});
+static_assert(b64decode("Zg==") == std::vector<uint8_t>{'f'});
+static_assert(b64decode("Zm8=") == std::vector<uint8_t>{'f','o'});
+static_assert(b64decode("Zm9v") == std::vector<uint8_t>{'f','o','o'});
+static_assert(b64decode("Zm9vYg==") == std::vector<uint8_t>{'f','o','o','b'});
+static_assert(b64decode("Zm9vYmE=") == std::vector<uint8_t>{'f','o','o','b','a'});
+static_assert(b64decode("Zm9vYmFy") == std::vector<uint8_t>{'f','o','o','b','a','r'});
