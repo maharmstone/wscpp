05c9c118a55d2d94aaf8015d24b063f0acb9ab3e
Mark Harmstone <mark@harmstone.com>
2024-05-13 14:40:21 +0100

    mod.cpp: fix for WIN32

--- src/mod.cpp
+++ src/mod.cpp
@@ -1,6 +1,10 @@
 export module wscpp;
 
+#ifdef _WIN32
+#define WSCPP __declspec(dllexport)
+#else
 #define WSCPP __attribute__ ((visibility ("default")))
+#endif
 
 namespace ws {
 	export int WSCPP foo() {
